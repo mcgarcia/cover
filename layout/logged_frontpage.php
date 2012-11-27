95a22367d76e2352a6bbb4cea86c8942eac9d33b
mcgarcia <mari.garcia.totnes@gmail.com>
2012-11-27 15:33:48 +0000

    Background images are not shown now. Border for blocks. Tyding a bit images in logged page

--- layout/logged_frontpage.php
+++ layout/logged_frontpage.php
@@ -3,19 +3,19 @@
 
 <div id="wrapper2">
   <a target="_blank" href="https://testlz.health.org.kw/my">
-  <img style="float:left; margin: 0 0 3% 0;" src="theme/cover/pix/my_learning_zone.jpg" alt="my learning zone" />
+  <img style="float:left; border: 1px solid #ccc; margin: -1% 0 3% 0;" src="theme/cover/pix/my_learning_zone.jpg" alt="my learning zone" />
   </a>
 
  <a target="_blank" href="https://testlz.health.org.kw/course/view.php?id=54">
-  <img style="float:left; margin: 0 0 3% 10px;"   src="theme/cover/pix/students_portal.jpg" alt="students portal" />
+  <img style="float:left; border: 1px solid #ccc; margin: -1% 0 3% 10px;"   src="theme/cover/pix/students_portal.jpg" alt="students portal" />
   </a>
   
   <a target="_blank" href="https://testlz.health.org.kw/course/view.php?id=54">
-  <img style="float:left; margin: 0 0 3% 10px;"   src="theme/cover/pix/library_resources.jpg" alt="library resources" />
+  <img style="float:left;  border: 1px solid #ccc; margin: -1% 0 3% 10px;"   src="theme/cover/pix/library_resources.jpg" alt="library resources" />
   </a>
   
   <a target="_blank" href="https://testlz.health.org.kw/course/view.php?id=54">
-  <img style="float:left; margin: 0 0 3% 10px;"   src="theme/cover/pix/getting_started.jpg" alt="getting started" />
+  <img style="float:left; border: 1px solid #ccc; margin: -1% 0 3% 10px;"   src="theme/cover/pix/getting_started.jpg" alt="getting started" />
   </a>
 
   
